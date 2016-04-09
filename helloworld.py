7e0807cbad82f4d4fd88e4906b511c3dcf99440f
Evinder <evindit@gmail.com>
2016-04-09 15:00:25 +0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,11 @@
-print ("hello world!")
-print ("hello world!")
-print ("hello world!")
-
+print ("Hello world! Your beaty is unforgettable! What's your name, my dear friend?")
+a=input()
+print("Such a pretty name, "+a+" !Do you want to continue our conversation, darling?")
+b=input()
+if b.find("No")>-1 or b.find("no")>-1:
+  print("Oh, you are so shy! I have never seen such honest and conservative people! In spite of your answer, I know that at the end you will be joyful and happy, "+a+". So tell me something about your hobbies, dear.")
+else:
+  print("Oh, my sun and stars! You can't even imagine my mood now! I've never felt such happines, "+a+". So, can you tell me anything about yourself?")
 
 
 
