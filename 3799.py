c3bbf2dd54cad4cbb9057d7971b729ebf2477b29
TTEPOTEXHUK <seny9538427610.s@yandex.com>
2016-04-09 14:31:05 +0300

    Update 3799.py

--- 3799.py
+++ 3799.py
@@ -1,2 +1,12 @@
 ﻿#http://informatics.mccme.ru/mod/statements/view3.php?id=3962&chapterid=3799#1
 #РЕКУРСИЯ!
+a = int(input())
+b=1
+while a>b:
+  if a//b
+    ans = "NO"
+    break
+  else:
+    b+=1
+    ans = 1"YES"
+print(c)
