424117c438aef47e72da38fe76d0ddce43097792
TTEPOTEXHUK <seny9538427610.s@yandex.com>
2016-04-16 14:40:03 +0300

    Убираем лишние строки

--- 197-1.py
+++ 197-1.py
@@ -1,6 +1,6 @@
 l = int(input())                        
 
-x = y = list(map(int, input().split()))
+x = list(map(int, input().split()))
 m = []
 a = list(range(l, 0, -1))
 for i in range(l-1, -1, -1):
