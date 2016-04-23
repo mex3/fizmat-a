3676220bb8bb2567e88b770e824ff4d8c53b3a41
trushkov_v <trushkov_v@fmsh2007>
2016-04-23 14:34:15 +0300

    #конструкция, создающая

--- 356-360.py
+++ 356-360.py
@@ -1,2 +1,9 @@
 #http://informatics.mccme.ru/mod/statements/view.php?id=15232#1
 #решить несколько задач из 5
+s = list(map(int, input().split()))
+m = s[0]
+n = s[1]
+A = [0] * m
+i = 0
+for i in range(m):
+    A[i] = [0] * n
