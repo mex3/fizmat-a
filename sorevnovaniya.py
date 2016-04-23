0b2f5b142b564087fe969d3b8221675bebdc53d0
Evinder <evindit@gmail.com>
2016-04-23 14:17:55 +0400

    Added files via upload

--- sorevnovaniya.py
+++ sorevnovaniya.py
@@ -1,11 +1,14 @@
 a, b = map(int, input().split())
 d=[]
+e=[]
 for c in range(a):
-    s=sum(list(map(int, input().split())))
+    i=list(map(int, input().split()))
+    s=max(i)
+    e.append(i.index(s))
     d.append(s)
 maxi=d[0]
 for c in d:
     if c>maxi:
         maxi=c
 print(maxi)
-print(d.index(maxi))
+print(d.index(maxi), e[d.index(maxi)])
